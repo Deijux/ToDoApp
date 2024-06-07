95338a149ab83962c86f8557a0a859734d8b4b4a
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-06-06 21:25:11 -0500

    Se eliminaron componentes no utilizados

--- src/app/components/TaskDone/TaskDone.tsx
+++
@@ -1,8 +0,0 @@
-export default function TaskDone(props: any) {
-  return (
-    <section className='text-white flex flex-col gap-3'>
-      <h1>Tareas realizadas - {props.count}</h1>
-      {props.children}
-    </section>
-  )
-}
--- src/app/components/TaskToDo/TaskToDo.tsx
+++
@@ -1,8 +0,0 @@
-export default function TaskToDo(props: any) {
-  return (
-    <section className='text-white flex flex-col gap-3'>
-      <h1>Tareas por hacer - {props.count}</h1>
-      {props.children}
-    </section>
-  )
-}
