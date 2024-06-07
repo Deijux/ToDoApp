ba68d191eb4a18ff208c975b6be55aaccb22c2e0
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-06-06 21:25:49 -0500

    Diferentes cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 'use client'
-import SearchBar from './components/SearchBar/SearchBar'
-import TaskDone from './components/TaskDone/TaskDone'
-import TaskToDo from './components/TaskToDo/TaskToDo'
+import SearchBar from '@/components/SearchBar/SearchBar'
+import TaskList from '@/components/TaskList/TaskList'
 import { servicesToDo } from './service/ToDo-service'
-import Card from './components/Card/Card'
+import Card from '@/components/Card/Card'
 
 import { useEffect, useState } from 'react'
 
 type Task = {
-  id: string | number
+  id: string
   title: string
   completed: boolean
 }
@@ -30,31 +29,63 @@ export default function Home() {
     setTasks([...tasks, newTask])
   }
 
+  const handleDeleteTask = (id: string) => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  const handleChangeStatusTask = (id: string) => {
+    setTasks(
+      tasks.map(task => {
+        if (task.id === id) {
+          task.completed = !task.completed
+        }
+        return task
+      }),
+    )
+  }
+
   return (
     <main className='flex flex-col gap-10'>
       <SearchBar onAddTask={handleNewTask} />
 
-      <TaskToDo count={countToDo}>
+      <TaskList count={countToDo}>
+        {countToDo > 0 && <h1>Tareas por hacer - {countToDo}</h1>}
         {tasks.map(task => {
           if (!task.completed) {
-            return <Card key={task.id} content={task.title} />
+            return (
+              <Card
+                id={task.id}
+                title={task.title}
+                key={task.id}
+                content={task.title}
+                onDeleteTask={handleDeleteTask}
+                onChangeStatusTask={handleChangeStatusTask}
+                completed={task.completed}
+              />
+            )
           }
         })}
-      </TaskToDo>
+      </TaskList>
 
-      <TaskDone count={countDone}>
+      <TaskList>
+        {countDone > 0 && <h1>Tareas realizadas - {countDone}</h1>}
         {tasks.map(task => {
           if (task.completed) {
             return (
               <Card
                 key={task.id}
+                id={task.id}
+                title={task.title}
                 content={task.title}
                 styles='line-through text-[#78CFB0]'
+                onDeleteTask={handleDeleteTask}
+                onChangeStatusTask={handleChangeStatusTask}
+                completed={task.completed}
               />
             )
           }
         })}
-      </TaskDone>
+      </TaskList>
     </main>
   )
 }
