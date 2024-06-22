95ae94562304a68fcd0b4a6e94717c820715197a
Juan Aguiar <jaguiarcruz17@gmail.com>
2024-06-22 10:58:48 -0500

    Se agrega variables de la api web y localhost

--- src/app/service/ToDo-service.ts
+++ src/app/service/ToDo-service.ts
@@ -1,11 +1,16 @@
+const localhost = 'http://localhost:3001/todoList'
+const apiWeb = 'https://my-json-server.typicode.com/deijux/ToDoApp-api/todoList'
+
+const apiFetch = apiWeb
+
 const getToDo = async () => {
-    const response = await fetch('http://localhost:3001/todoList');
+    const response = await fetch(apiFetch);
     const data = await response.json();
     return data;
 }
 
 const addTask = async (title: string) => {
-    const response = await fetch('http://localhost:3001/todoList', {
+    const response = await fetch(apiFetch, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -18,7 +23,7 @@ const addTask = async (title: string) => {
 }
 
 const deleteTask = async (id: string) => {
-    const response = await fetch(`http://localhost:3001/todoList/${id}`, {
+    const response = await fetch(apiFetch + '/' + id, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
@@ -29,7 +34,7 @@ const deleteTask = async (id: string) => {
 }
 
 const changeStatusTask = async (id: string, title: string, completed: boolean) => {
-    const response = await fetch(`http://localhost:3001/todoList/${id}`, {
+    const response = await fetch(apiFetch + '/' + id, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
