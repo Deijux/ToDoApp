7e64b6324c29952dfff790eece28a99478d3b8ec
Juan Aguiar <jaguiarcruz17@gmail.com>
2026-01-17 21:57:17 -0500

    feat(api): Add interceptors

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,3 +4,24 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 })
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    if (axios.isAxiosError(error) && error.response) {
+      const { status } = error.response
+      const { pathname } = window.location
+
+      if (status === 401 && pathname !== '/login') {
+        try {
+          await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {}, { withCredentials: true })
+        } catch (error) {
+          console.error('Error during logout request:', error)
+        } finally {
+          window.location.href = '/login'
+        }
+      }
+    }
+    return Promise.reject(error)
+  },
+)
