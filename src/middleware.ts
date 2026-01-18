09cb1bc5afe71693ddb9f93b672452c488654b3b
Juan Aguiar <jaguiarcruz17@gmail.com>
2026-01-17 22:23:56 -0500

    feat(middleware): Add a middleware

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('access_token')?.value
+
+  const { pathname } = request.nextUrl
+
+  if (pathname.startsWith('/tasks') && !token) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  if (pathname === '/login' && token) {
+    return NextResponse.redirect(new URL('/tasks', request.url))
+  }
+
+  if (pathname === '/' && token) {
+    return NextResponse.redirect(new URL('/tasks', request.url))
+  }
+
+  if (pathname === '/' && !token) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  return NextResponse.next() 
+}
+
+export const config = {
+  matcher: [
+    '/tasks/:path*',
+    '/login',
+    '/',
+  ],
+}
